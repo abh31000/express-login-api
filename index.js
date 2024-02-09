94b5c9b87b83c1fd106e639a8b205c1aeff7cd58
abh31000 <abdelkhalekbouklihacene67@gmail.com>
2024-02-09 19:04:38 +0100

    Rewrote responses for first vuln lab

--- index.js
+++ index.js
@@ -18,31 +18,31 @@ app.post('/api/vuln1/signup', (req, res) => {
     const {username, password} = req.body
 
     if(!username || !password){
-        res.status(400).json({error: "Username and password are required for signing up"})
+        res.status(400).send("Username and password are required for signing up")
     }
 
     const existing = users1.find((elem) => elem.username === username)
     if(existing){
-        res.status(400).json({error: "Username already exists"})
+        res.status(400).send("Username already exists")
     }
 
     users1.push({username, password, isAdmin: "false"})
-    res.status(200).json({message: "User created successfully"})
+    res.status(200).send("User created successfully")
 })
 
 app.post('/api/vuln1/login', (req, res) => {
     const {username, password} = req.body
 
     if(!username || !password){
-        res.status(400).json({error: "Username and password are required for logging in"})
+        res.status(400).send("Username and password are required for logging in")
     }
 
     const auth = users1.find((elem) => elem.username === username)
     if(!auth || auth.password !== password){
-        res.status(401).json({error: "Invalid username or password"})
+        res.status(401).send("Invalid username or password")
     }
     
-    res.status(200).json({message: "Login successful"})
+    res.status(200).send("Login successful")
 })
 
 app.get('/api/vuln1/admin', (req, res) => {
@@ -53,7 +53,7 @@ app.get('/api/vuln1/admin', (req, res) => {
     
     const test = users1.find((elem) => elem.username === username)
     if(!test || test.isAdmin === "false"){
-        res.status(401).json({error: "You don't have permission"})
+        res.status(401).send("You don't have permission")
     }
 })
 
