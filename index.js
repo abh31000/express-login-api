10d68f421a83cd135acd6ed4ae79dc906cdeb878
abh31000 <abdelkhalekbouklihacene67@gmail.com>
2024-02-09 17:12:50 +0100

    Introduce main API endpoints

--- index.js
+++ index.js
@@ -4,7 +4,24 @@ const app = express()
 
 app.get('/', (req, res) => {
     const ipAddress = requestIp.getClientIp(req)
-    res.send(`Hello there, your IP adress: ${ipAddress}`)
+    res.send(`Hello there, your IP address: ${ipAddress}`)
 })
+
+
+// Vulnerabilité 1
+app.get('/api/vuln1', (res) => {
+    res.send('API endpoint for first vulnerablity.')
+})
+
+// Vulnerabilité 2
+app.get('/api/vuln2', (res) => {
+    res.send('API endpoint for second vulnerablity.')
+})
+
+// Vulnerabilité 3
+app.get('/api/vuln3', (res) => {
+    res.send('API endpoint for third vulnerablity.')
+})
+
 
 module.exports = app
